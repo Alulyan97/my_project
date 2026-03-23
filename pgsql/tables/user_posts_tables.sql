12428b231ebe0b066d5a9809ec0f2f1d100e8b8f
Alulyan Petros <alulyanp@mail.ru>
2026-03-24 00:09:42 +0300

    работа с таблицами

--- pgsql/tables/user_posts_tables.sql
+++ pgsql/tables/user_posts_tables.sql
@@ -1,6 +1,6 @@
 -- Создание таблиц
 CREATE TABLE users (
-    id VARCHAR(80),
+    id VARCHAR(80) PRIMARY KEY,
     name VARCHAR(100),
     email VARCHAR(255)
 );
@@ -35,3 +35,17 @@ SELECT * FROM users JOIN posts ON users.id = posts.users_id;
 SELECT users_id, COUNT(*) AS posts_count
 FROM posts
 GROUP BY users_id;
+
+-- Создаем другую таблицу с тимами данних и ограничениями 
+CREATE TABLE orders (
+    id VARCHAR(80) PRIMARY KEY,
+    users_id VARCHAR(80) REFERENCES users(id),
+    amount NUMERIC CHECK(amount > 0),
+    status VARCHAR DEFAULT 'pending',
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+    meta JSONB
+);
+
+-- Заполняем таблицу orders кривыми данными, чтоб увидеть ошибку 
+INSERT INTO orders (id, users_id, amount) VALUES
+('o3','u3', -100)
